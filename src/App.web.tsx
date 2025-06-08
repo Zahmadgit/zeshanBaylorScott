5de0d8f2c5d44bd57ade4a3099bfc00c8bfdb3d8
Zeshan Ahmad <zahmad5870@gmail.com>
2025-06-08 00:40:36 -0400

    current working order

---
+++ src/App.web.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView style={styles.sectionContainer}>
+      <Text>Yay its working</Text>
+      <Text>BRUH WORKS WEB</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+});
+
+export default App;
